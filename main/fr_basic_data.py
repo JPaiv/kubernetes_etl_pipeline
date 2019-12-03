0d1f3ad9ca09f4755cc69b4f540079efa044af0a
JPaiv <30726520+JPaiv@users.noreply.github.com>
2019-12-03 15:50:50 +0200

    Update fr_basic_data.py

--- main/fr_basic_data.py
+++ main/fr_basic_data.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env
 # -*- coding: utf-8 -*-
+
 import sys
-#pylint: disable=import-error, no-name-in-module
-from nightcrawlers.utils.pandas_tools.dataframe_chunkers import chunk_csv_to_s3_create_kubejob_for_each_chunk
-from nightcrawlers.france.fr_helpers import define_kubernetes_arguments, download_data_file
-#import pdb; pdb.set_trace()
+from utils.pandas_tools.dataframe_chunkers import chunk_csv_to_s3_create_kubejob_for_each_chunk
+from main.helpers import (
+    define_kubernetes_arguments,
+    download_data_file
+    )
 
 
 source = sys.argv[1]
 
 
-def basic_data():
+def main():
     data_file = download_data_file(source)
-
     kubernetes_args = define_kubernetes_arguments(source)
-
     chunk_csv_to_s3_create_kubejob_for_each_chunk(source, data_file, kubernetes_args, limits={'cpu': '1', 'memory': '4Gi'}, requests={'cpu': '1', 'memory': '2Gi'})
 
 
 if __name__ == "__main__":
-    basic_data()
+    main()
