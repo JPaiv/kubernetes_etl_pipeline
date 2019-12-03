7f963a73bbcb8caa89f13375a3e798a0960fed09
JPaiv <30726520+JPaiv@users.noreply.github.com>
2019-12-03 15:56:28 +0200

    Update legale_parse.py

--- main/legale_parse.py
+++ main/legale_parse.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env
 # -*- coding: utf-8 -*-
 import sys
-#pylint: disable=import-error, no-name-in-module
-from nightcrawlers.france.fr_helpers import \
-    fr_create_dataframe, \
-    remove_empty_values, \
-    give_form_of_company, \
-    legal_category_download
 
-from nightcrawlers.france.basic_data.fr_mappings import mappings
-from nightcrawlers.utils.dask.pandas_to_dask import iterate_dataframe
-from nightcrawlers.france.basic_data.legale_post_process import post_process_fr_legale
-from nightcrawlers.utils.pandas_tools.dataframe_tools import map_row_values
-from nightcrawlers.utils import send_prospect_sources
+from nightcrawlers.france.fr_helpers import (
+    fr_create_dataframe,
+    remove_empty_values,
+    give_form_of_company,
+    legal_category_download
+    )
+
+from utils.pandas.dataframe_tools import (
+    map_row_values,
+    pandas_dataframe
+    )
+
+from main.mappings import mappings
+from main.legale_post_process import post_process_legale
+from utils.dask.pandas_to_dask import iterate_dataframe
+from utils import send_prospect_sources
 
 
 legale_mapping = mappings['fr_legale']
-source = 'fr_legale'
+source = 'legale'
 
 download_index = sys.argv[1]
 
 
 def parse():
-    dataframe = fr_create_dataframe(download_index, source)
+    dataframe = pandas_dataframe(download_index, source)
     legal_category_data = legal_category_download()
     prospects = iterate_dataframe(dataframe, parse_row)
 
@@ -43,4 +48,4 @@ def parse_row(row):
 
 if __name__ == "__main__":
     prospect = parse()
-    send_prospect_sources('fr_legale_data', 'NDQ0N89NP3o07PGsqn58AIDA5ZQfOLk6md31_edTx5N5Lz0vcIWsirkuesvHCuLhOipNfP3LCgUs4X95qIqreluMyu8M', prospect)
+    send_prospect_sources('fr_legale_data', os.environ['SEND_KEY', prospect)
